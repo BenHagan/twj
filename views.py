1ac32ba7130a80658c8e7f2f43a8540c394fb21e
Ben Hagan <BenHagan@users.noreply.github.com>
2016-04-28 13:31:05 -0500

    Chapter 4 Complete

--- views.py
+++ views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(
+                        """
+                        Rango says hello world!<br />
+                        <a href="/rango/about/">About</a>
+                        """
+                        )
+
+
+def about(request):
+    return HttpResponse(
+                        """
+                        Rango says here is the about page. <br />
+                        <a href="/rango/">Home</a>
+                        """
+                        )

abae937f14dd9fc3b420e5b8b642941ba058905c
Ben Hagan <BenHagan@users.noreply.github.com>
2016-04-29 00:32:06 -0500

    Chapter 7 Complete

--- views.py
+++ views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 
 def index(request):
-    context_dict = {'boldmessage': "I am a bold font from the context"}
-    return render(request, 'rango/index.html', context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context = {'categories': category_list, 'pages': page_list}
+
+    return render(request, 'rango/index.html', context)
 
 
 def about(request):
     context = {'boldmessage': 'This is my about page.'}
     return render(request, 'rango/about.html', context)
+
+
+def category(request, category_name_slug):
+
+    context = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context['category'] = category
+
+        pages = Page.objects.filter(category=category)
+        context['pages'] = pages
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context)
